ded2327e4b271e9cae8d45711206889e08c279cc
Qinme <yaqin.gawean@gmail.com>
2023-06-05 13:34:41 +0700

    commit pertama

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Hal1 from './Component/Hal1';
+
+function App() {
+  return (
+
+    <Hal1/>
+
+    );
+}
+
+export default App;
---
+++ my-app/src/Component/Hal1.js
@@ -0,0 +1,9 @@
+function Hal1() {
+  return (
+
+    <h1>ini halaman 1</h1>
+
+    );
+}
+
+export default Hal1;
